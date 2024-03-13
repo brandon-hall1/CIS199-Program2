614057d22f46477e90f8ff9d38888ecadda20a2b
unknown <brandonthall3@gmail.com>
2024-03-12 20:16:03 -0400

    First commit

---
+++ Program2/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Program2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guestsLabel = new System.Windows.Forms.Label();
+            this.nightsLabel = new System.Windows.Forms.Label();
+            this.starsLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.costLabel = new System.Windows.Forms.Label();
+            this.costOutputLabel = new System.Windows.Forms.Label();
+            this.guestsInputTextBox = new System.Windows.Forms.TextBox();
+            this.nightsInputTextBox = new System.Windows.Forms.TextBox();
+            this.starsInputComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // guestsLabel
+            // 
+            this.guestsLabel.AutoSize = true;
+            this.guestsLabel.Location = new System.Drawing.Point(50, 30);
+            this.guestsLabel.Name = "guestsLabel";
+            this.guestsLabel.Size = new System.Drawing.Size(95, 13);
+            this.guestsLabel.TabIndex = 0;
+            this.guestsLabel.Text = "Number of Guests:";
+            // 
+            // nightsLabel
+            // 
+            this.nightsLabel.AutoSize = true;
+            this.nightsLabel.Location = new System.Drawing.Point(50, 70);
+            this.nightsLabel.Name = "nightsLabel";
+            this.nightsLabel.Size = new System.Drawing.Size(92, 13);
+            this.nightsLabel.TabIndex = 1;
+            this.nightsLabel.Text = "Number of Nights:";
+            // 
+            // starsLabel
+            // 
+            this.starsLabel.AutoSize = true;
+            this.starsLabel.Location = new System.Drawing.Point(50, 108);
+            this.starsLabel.Name = "starsLabel";
+            this.starsLabel.Size = new System.Drawing.Size(62, 13);
+            this.starsLabel.TabIndex = 2;
+            this.starsLabel.Text = "Hotel Stars:";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(123, 143);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // costLabel
+            // 
+            this.costLabel.AutoSize = true;
+            this.costLabel.Location = new System.Drawing.Point(50, 185);
+            this.costLabel.Name = "costLabel";
+            this.costLabel.Size = new System.Drawing.Size(59, 13);
+            this.costLabel.TabIndex = 4;
+            this.costLabel.Text = "Hotel Cost:";
+            // 
+            // costOutputLabel
+            // 
+            this.costOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.costOutputLabel.Location = new System.Drawing.Point(174, 184);
+            this.costOutputLabel.Name = "costOutputLabel";
+            this.costOutputLabel.Size = new System.Drawing.Size(100, 19);
+            this.costOutputLabel.TabIndex = 5;
+            this.costOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // guestsInputTextBox
+            // 
+            this.guestsInputTextBox.Location = new System.Drawing.Point(174, 27);
+            this.guestsInputTextBox.Name = "guestsInputTextBox";
+            this.guestsInputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.guestsInputTextBox.TabIndex = 6;
+            // 
+            // nightsInputTextBox
+            // 
+            this.nightsInputTextBox.Location = new System.Drawing.Point(174, 67);
+            this.nightsInputTextBox.Name = "nightsInputTextBox";
+            this.nightsInputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nightsInputTextBox.TabIndex = 7;
+            // 
+            // starsInputComboBox
+            // 
+            this.starsInputComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.starsInputComboBox.FormattingEnabled = true;
+            this.starsInputComboBox.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.starsInputComboBox.Location = new System.Drawing.Point(174, 105);
+            this.starsInputComboBox.Name = "starsInputComboBox";
+            this.starsInputComboBox.Size = new System.Drawing.Size(100, 21);
+            this.starsInputComboBox.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 237);
+            this.Controls.Add(this.starsInputComboBox);
+            this.Controls.Add(this.nightsInputTextBox);
+            this.Controls.Add(this.guestsInputTextBox);
+            this.Controls.Add(this.costOutputLabel);
+            this.Controls.Add(this.costLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.starsLabel);
+            this.Controls.Add(this.nightsLabel);
+            this.Controls.Add(this.guestsLabel);
+            this.Name = "Form1";
+            this.Text = "Program 2 - Hotel Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label guestsLabel;
+        private System.Windows.Forms.Label nightsLabel;
+        private System.Windows.Forms.Label starsLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label costLabel;
+        private System.Windows.Forms.Label costOutputLabel;
+        private System.Windows.Forms.TextBox guestsInputTextBox;
+        private System.Windows.Forms.TextBox nightsInputTextBox;
+        private System.Windows.Forms.ComboBox starsInputComboBox;
+    }
+}
+
---
+++ Program2/Form1.cs
@@ -0,0 +1,152 @@
+﻿/*
+ * Grading ID: K3194
+ * Program 2
+ * Due Date: 3/10/2022
+ * Course Section: CIS 199-01
+ * Description: Calculates the total hotel stay cost based on number of guests, number of nights, and number of stars
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e) //event handler for calculateButton click
+        {
+            const int ONE_GUEST_FLAT_FEE = 100; //flat fee for 1 guest
+            const int TWO_GUESTS_FLAT_FEE = 150; //flat fee for 2 guests
+            const int THREE_GUESTS_FLAT_FEE = 250; //flat fee for 3 guests
+            const int FOUR_TO_SEVEN_GUESTS_FLAT_FEE = 400; //flat fee for 4-7 guests
+
+            const int DAILY_RATE = 100; //nightly rate for 1-6 nights
+            const int WEEKLY_RATE = 75; //nightly rate for 7-30 nights
+            const int MONTHLY_RATE = 25; //nightly rate for 31+ nights
+
+            const double ONE_STAR_MULTIPLIER = 1.0; //rate multiplier for 1 star hotel
+            const double TWO_STAR_MULTIPLIER = 1.5; //rate multiplier for 2 star hotel
+            const double THREE_STAR_MULTIPLIER = 2.5; //rate multiplier for 3 star hotel
+            const double FOUR_STAR_MULTIPLIER = 3.0; //rate multiplier for 4 star hotel
+            const double FIVE_STAR_MULTIPLIER = 4.0; //rate multiplier for 5 star hotel
+
+            bool invalidInput = false; //flag for tracking if any inputs were invalid
+            int flatFee = 0; //int to hold flat fee based on numGuests
+            int nightlyRate = 0; //int to hold nightly rate based on numNights
+            double starMultiplier = 0; //double to hold star multiplier based on numStars
+
+            if(int.TryParse(guestsInputTextBox.Text, out int numGuests)) //attempt to parse guests input and store in numGuests
+            {
+                if(numGuests < 1 || numGuests > 7) //check if value outside of valid numGuests range (1-7)
+                {
+                    guestsInputTextBox.Text = "INVALID INPUT"; //set text of guestsInputTextBox to INVALID INPUT
+                    invalidInput = true; //set flag to true if invalid input detected
+                }
+                else //if numGuests is within valid range
+                {
+                    if(numGuests == 1)  //set flatFee to corresponding fee based on numGuests
+                    {
+                        flatFee = ONE_GUEST_FLAT_FEE; 
+                    }
+                    else if (numGuests == 2)
+                    {
+                        flatFee = TWO_GUESTS_FLAT_FEE;
+                    }
+                    else if (numGuests == 3)
+                    {
+                        flatFee = THREE_GUESTS_FLAT_FEE;
+                    }
+                    else
+                    {
+                        flatFee = FOUR_TO_SEVEN_GUESTS_FLAT_FEE;
+                    }
+                }
+            } 
+            else //if parsing guests input results in error
+            {
+                guestsInputTextBox.Text = "INVALID INPUT";
+                invalidInput = true;
+            }
+
+            if(int.TryParse(nightsInputTextBox.Text, out int numNights)) //attempt to parse nights input and store in numNights
+            {
+                if(numNights < 1) //check if value outside of valid numNights range (1+)
+                {
+                    nightsInputTextBox.Text = "INVALID INPUT";
+                    invalidInput = true;
+                }
+                else //if numNights is within valid range
+                {
+                    if(numNights <= 6) //set nightlyRate to corresponding rate based on numNights
+                    {
+                        nightlyRate = DAILY_RATE;
+                    }
+                    else if (numNights <= 30)
+                    {
+                        nightlyRate = WEEKLY_RATE;
+                    }
+                    else
+                    {
+                        nightlyRate = MONTHLY_RATE;
+                    }
+                }
+            }
+            else //if parsing nights input results in error
+            {
+                nightsInputTextBox.Text = "INVALID INPUT";
+                invalidInput = true;
+            }
+
+            if(int.TryParse(starsInputComboBox.Text, out int numStars)) //attempt to parse stars input and store in numStars
+            {
+                if(numStars == 1) //set starMultiplier to corresponding multiplier based on numStars
+                {
+                    starMultiplier = ONE_STAR_MULTIPLIER;
+                }
+                else if (numStars == 2)
+                {
+                    starMultiplier = TWO_STAR_MULTIPLIER;
+                }
+                else if (numStars == 3)
+                {
+                    starMultiplier = THREE_STAR_MULTIPLIER;
+                }
+                else if (numStars == 4)
+                {
+                    starMultiplier = FOUR_STAR_MULTIPLIER;
+                }
+                else
+                {
+                    starMultiplier = FIVE_STAR_MULTIPLIER;
+                }
+            }
+            else //if parsing stars input results in error (user left blank)
+            {
+                starsInputComboBox.Text = ""; //ensure starsInput is blank
+                costOutputLabel.Text = "INVALID STARS"; //set costOutputLabel to display error (can't set text of exclusive list combo box)
+                return; //end execution of function to avoid changing costOutputLabel from INVALID STARS
+            }
+
+            if (invalidInput) //if invalid input was detected on # of guests or # of nights
+            {
+                costOutputLabel.Text = "INVALID INPUT"; //set costOutputLabel to INVALID INPUT if any inputs were invalid
+                return; //end execution of function to avoid changing costOutputLabel from INVALID INPUT
+            }
+
+            double hotelCost = (flatFee + (numNights * nightlyRate)) * starMultiplier; //perform hotel cost calculation
+            costOutputLabel.Text = hotelCost.ToString("C"); //set costOutputLabel to hotelCost with currency formatting
+        }
+    }
+}
